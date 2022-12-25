39d5485a4e4bd2eed6084cb28b203587ab7974bb
CarinaNovais <carinapereiran@outlook.com>
2022-12-25 18:44:17 -0300

    fix bugs js

--- script.js
+++ script.js
@@ -10,18 +10,21 @@ var textoRes = document.querySelector('#res'); //textarea
 
 var display01 = document.querySelector('#conteudoAnterior');
 
+textoRes.value = ' ';
+
 function codificar(){
     display01.style.display = 'none';
     var entrada = msgInput.value;
-    var novaSaida = entrada.replace(/e/g,'enter').replace(/i/g,'imes').replace(/a/g,'ai').replace(/o/g,'ober').replace(/u/g,'ufat').replace(' ', ' ');
+    var novaSaida = entrada.replace(/e/g,'eX').replace(/i/g,'is').replace(/a/g,'ai').replace(/o/g,'oz').replace(/u/g,'um').replace(' ', ' ');
     textoRes.value = novaSaida;
     return novaSaida
     console.log(novaSaida);
 }
 
 function decodificar(){
+    display01.style.display = 'none';
     var novaEntrada = msgInput.value;
-    var descodigo = novaEntrada.replace(/enter/g,'e').replace(/imes/g,'i').replace(/ai/g,'a').replace(/ober/g,'o').replace(/ufat/g,'u').replace(' ', ' ');
+    var descodigo = novaEntrada.replace(/eX/g,'e').replace(/is/g,'i').replace(/ai/g,'a').replace(/oz/g,'o').replace(/um/g,'u').replace(' ', ' ');
     textoRes.value = descodigo;
     return descodigo
     console.log(descodigo);
